a56bf59cc0a4d4eb805945b8e2e7a904992ef739
dsbaek2020 <dsbaek898@gmail.com>
2023-10-22 10:09:54 +0900

    코드 저장

--- minji/caterpillar/main.py
+++ minji/caterpillar/main.py
@@ -9,3 +9,13 @@ caterpillar.color('green')
 caterpillar.speed(0)
 caterpillar.penup()
 caterpillar.hideturtle()
+
+leaf = t.Turtle()
+leaf_shape = ((0, 0), (14, 2), (18, 6), (20, 20), \
+              (6, 18), (2, 14))
+t.register_shape('leaf', leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.penup()
+leaf.hideturtle()
+leaf.speed(0)
