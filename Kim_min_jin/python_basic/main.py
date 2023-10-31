d64ed6c838aea8fbced44a81e34513715710b964
dsbaek2020 <dsbaek898@gmail.com>
2023-10-31 17:10:12 +0900

    준언이 파이썬 기본 연습

---
+++ Kim_min_jin/python_basic/main.py
@@ -0,0 +1,5 @@
+
+name = "Soon Shin Lee"
+print(f'-----------------------')
+print(f'---Hi, {name}---')
+print(f'-----------------------')
---
+++ juneon/pythonProject/main.py
@@ -0,0 +1,11 @@
+print('안녕')
+answer = input('뭐먹었니?')
+print('오늘 '+answer+'먹었어')
+7+3
+7-3
+7/3
+7*3
+#while True:
+    #print('jump')
+for i in range (10):
+    print('jump')
