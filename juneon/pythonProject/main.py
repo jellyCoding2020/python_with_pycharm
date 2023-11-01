856e212fa0ec82bd79162d0c3746870e79e860d2
dsbaek2020 <dsbaek898@gmail.com>
2023-11-01 14:03:15 +0900

    준언이 파이썬 연습 코드 저장

--- juneon/pythonProject/main.py
+++ juneon/pythonProject/main.py
@@ -1,11 +1,21 @@
-print('안녕')
+roll = 2
+happy = "안녕"
+my_question = '넌 이름이 뭐니?'
+answer = input(my_question)
+
+print('안녕', answer+'야')
 answer = input('뭐먹었니?')
 print('오늘 '+answer+'먹었어')
-7+3
-7-3
-7/3
-7*3
+print('7+3 은 ' + str(7+3))
+a = 7-3
+print('7-3 은 ' + str(a))
+print('7/3 은 ' + str(7/3))
+print('7*3 은 ' + str(7*3))
 #while True:
     #print('jump')
 for i in range (10):
     print('jump')
+
+for i in range (10):
+    if roll == 2:
+        print('hi')
