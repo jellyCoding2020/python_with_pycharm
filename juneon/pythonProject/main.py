4884ff8b0372ae0e582e6d60bc48f2bad6fa7eb6
dsbaek2020 <dsbaek898@gmail.com>
2023-11-09 17:52:18 +0900

    준언이 자료형 연습

--- juneon/pythonProject/main.py
+++ juneon/pythonProject/main.py
@@ -1,9 +1,27 @@
+
+fruit_name = []
+
+for i in range (5):
+    f_name = input('과일이름하나만 입력해 주세요:  ')
+    fruit_name.append(f_name)
+
+print(fruit_name)
+
+
+
 roll = 2
 happy = "안녕"
 my_question = '넌 이름이 뭐니?'
 answer = input(my_question)
 
+
 print('안녕', answer+'야')
+
+print(answer + '씨 ' +fruit_name[3] + fruit_name[2] + ' 주스나왔습니다.')
+
+
+
+
 answer = input('뭐먹었니?')
 print('오늘 '+answer+'먹었어')
 print('7+3 은 ' + str(7+3))
@@ -19,3 +37,5 @@ for i in range (10):
 for i in range (10):
     if roll == 2:
         print('hi')
+
+
