636b376f617c095ad70efc053047080d7e20a949
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2023-12-13 16:02:23 +0900

    민지 퀴즈 게임 저장(완성)

--- minji/quiz game/quiz game.py
+++ minji/quiz game/quiz game.py
@@ -1,23 +1,24 @@
 from guizero import App, Box, PushButton, Text, TextBox
 
-app = App()
+app = App(width=700)
 
 
 questions_bio = ['다음 중 음식물이 직접 지나가지 않는 소화 기관은 무엇인가?',
                  '다음 중 녹말 분해 효소는 무엇인가?',
                  '다음 중 호흡 기관에 포함되지 않는 것은?',
-                 '다음 중 배설과정과  관련이 없는 것은?']
+                 '다음 중 배설과정과  관련이 없는 것은?',
+                 '다음 중 동물에겐 있지만 식물에겐 없는 것은?']
 
-answer1 = ['입', '트립신', '폐', '콩팥']
-answer2 = ['위', '펩신', '기관', '오줌관']
-answer3 = ['소장', '라이페이스', '코', '세뇨관']
-answer4 = ['간', '아밀레이스', '입', '대장']
+answer1 = ['입', '트립신', '폐', '콩팥', '세포']
+answer2 = ['위', '펩신', '기관', '오줌관', '개체']
+answer3 = ['소장', '라이페이스', '코', '세뇨관', '기관']
+answer4 = ['간', '아밀레이스', '입', '대장', '기관계']
 
-questions_hist = ['과거 송나라의 황제가 무신을 무시하고 문신을 우대한 정책은?(영어로 바꾸어서 쓰시오)',
-                  '세상에서 가장 넓은 땅을 보유했었던 몽골 제국의 첫번째 왕은?(영어로 바꾸어서 쓰시오)',
-                  '최초로 세계 일주를 성공한 사람은?(영어로 바꾸어서 쓰시오)']
-answer_description = ['ansclwndml', 'cldrlwmzks', 'akwpffks']
-
+questions_hist = ['과거 송나라의 황제가 무신을 무시하고 문신을 우대한 정책은?',
+                  '세상에서 가장 넓은 땅을 보유했었던 몽골 제국의 첫번째 왕은?',
+                  '최초로 세계 일주를 성공한 사람은?',
+                  '아메리카 대륙을 최초로 발견한 사람은?']
+answer_description = ['문치주의', '칭기즈칸', '마젤란', '콜럼버스']
 
 class quiz():
     def __init__(self, quiz, ans1,ans2,ans3,ans4, type, answer):
@@ -53,14 +54,20 @@ class quiz():
             self.button3.tk.grid(padx=30, pady=10)
             self.button4.tk.grid(padx=10, pady=20)
         else :
-            self.answer = TextBox(self.button_box, command=self.check_answer,  grid=[1, 0])
+            self.userAnswer = TextBox(self.button_box, grid=[1, 0])
 
         self.button5 = PushButton(self.button_box, text="다음 문제", grid=[4, 10], command=self.next_qna)
         self.button6 = PushButton(self.button_box, text="이전 문제", grid=[1, 10], command=self.pior_qna)
         self.button7 = PushButton(self.button_box, text="확인하기", grid=[6, 10], command=self.check_answer)
         #self.check_button1()
 
-
+    def check_answer(self):
+        print("bbbbb")
+        if self.userAnswer.value == self.answer[self.question_number - 1]:
+            print("텍스트 상자의 내용:", self.userAnswer)
+            self.correct()
+        else:
+            self.wrong()
 
     def correct(self):
         self.message.value = "정답!"
@@ -70,19 +77,13 @@ class quiz():
         self.message.value = "오답!"
         self.message.text_color = "red"
 
-    def check_answer(self):
-        if self.button7.enabled:
-            print("bbbbb")
-            if self.answer == self.answer.value[self.question_number - 1]:
-                print("텍스트 상자의 내용:", self.answer)
-                self.correct()
-            else:
-                self.wrong()
+
 
 
     def next_qna(self):
         self.question_number += 1
         self.message.value = self.questions[self.question_number-1]
+        self.userAnswer.value = ''
         if type == 1:
             self.button1.text = self.answer1[self.question_number-1]
             self.button2.text = self.answer2[self.question_number - 1]
