fcc1d7e0d07262ba84a9fb0172b038a8ab7e4e16
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2024-02-28 15:51:04 +0900

    BACKUP SAVE

---
+++ sort array-KMJ.py/prime_number.py
@@ -0,0 +1,18 @@
+
+
+prime_number = []
+
+def find_prime(s, e):
+    for i in range(s, e+1):
+        print('This is', i)
+
+
+        prime_number.append(i)
+
+    print(prime_number)
+
+
+
+
+
+find_prime(1, 5)
