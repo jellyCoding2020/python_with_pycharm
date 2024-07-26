2d8adf113a6f72f6893f6bd1d551afc5d9bf945c
dsbaek2020 <dsbaek898@gmail.com>
2024-07-26 14:34:44 +0900

    noel pile

--- noelcaterpillar/main.py
+++ noelcaterpillar/main.py
@@ -5,8 +5,8 @@ def close_window():
     window.bye()
 
 window.listen()
-window.onkeypress(close_window)
-
+#window.onkeypress(close_window)
+# 애벌래의  모양과 형태를 정하는 코드 (9~15)
 caterpillar=t.Turtle()
 caterpillar.shape('square')
 caterpillar.color('blue')
@@ -14,7 +14,7 @@ caterpillar.speed(0)
 caterpillar.penup()
 caterpillar.hideturtle()
 
-
+#잎의 모양과 형태를 정하는 코드(17~26)
 leaf = t.Turtle()
 leaf_shape = ((0, 0), (14, 2), (18, 6), (20, 20),
               (6, 18), (2, 14))
@@ -27,6 +27,7 @@ leaf.speed(0)
 
 
 game_started = False
+#창에  글자를 보이게 하려는 코드(31~34)
 text_turtle=t.Turtle()
 text_turtle.write('press SPACE', align='center',
                   font=('arial',16,'bold'))
@@ -37,7 +38,14 @@ score_turtle.hideturtle()
 score_turtle.speed(0)
 
 def outside_window():
-    pass
+    left_wall = -t.window_width() / 2
+    right_wall = t.window_width() / 2
+    top_wall = t.window_height() / 2
+    bottom_wall = -t.window_height() / 2
+    (x, y)=caterpillar.pos()
+    outside =   x<left_wall or x>right_wall or y<bottom_wall or y>top_wall
+    return outside
+
 def game_over():
     pass
 def display_score(current_score):
@@ -45,6 +53,7 @@ def display_score(current_score):
 def place_leaf():
     pass
 def start_game():
+    #게임이 시작됐는지 확인 하는 코드(50~53)
     global game_started
     if game_started:
         return
@@ -52,7 +61,7 @@ def start_game():
 
     score = 0
     text_turtle.clear()
-
+#애벌레의 크기를 정하는 코드
     caterpillar_speed = 2
     caterpillar_length = 3
     caterpillar.shapesize(1,caterpillar_length,1)
